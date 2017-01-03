26be8950f2e177dbe55be9dfef91d6ee9617e8ef
Tradezap Apple1 <apple1@Tradezaps-iMac-Developer.local>
2017-01-03 19:05:03 +0530

    Included the Loader Module.

---
+++ simploader/src/main/java/com/proasynclodr/MyApiLoader.java
@@ -0,0 +1,147 @@
+package com.proasynclodr;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by Mohanraj.S on 03/01/17.
+ */
+
+public class MyApiLoader extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+    TextView tvJsonResult;
+    ScrollView scrl_json;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myloader);
+        tvJsonResult = (TextView) findViewById(R.id.tv_json_result);
+        scrl_json = (ScrollView) findViewById(R.id.scrl_json);
+        getSupportLoaderManager().initLoader(0, null, (LoaderManager.LoaderCallbacks<String>)this).forceLoad();
+
+        if (scrl_json != null) {
+
+            /*scrl_json.setOnScrollChangeListener(new ScrollView.OnScrollChangeListener() {
+                @Override
+                public void onScrollChange(ScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+
+                    if (scrollY == 0) {
+
+                    }
+
+                    if (scrollY == (v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight())) {
+                        Log.i(TAG, "BOTTOM SCROLL");
+
+
+
+                    }
+                }
+            });*/
+
+            /*scrl_json.setOnScrollChangeListener(new ScrollView.OnScrollChangeListener() {
+                @Override
+                public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                    if (scrollY == 0) {
+
+                    }
+
+                    if (scrollY == (v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight())) {
+                        Log.i(TAG, "BOTTOM SCROLL");
+
+
+
+                    }
+                }
+            });*/
+        }
+
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return new FetchData(this);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+
+        tvJsonResult.setText(data);
+
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
+    }
+
+    private static class FetchData extends AsyncTaskLoader<String> {
+
+        public FetchData(Context context) {
+            super(context);
+        }
+
+        @Override
+        public String loadInBackground() {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = null;
+            String line;
+            try {
+                URL url = new URL("https://itunes.apple.com/search?term=classic");
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) return null;
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                while ((line = reader.readLine()) != null) buffer.append(line);
+
+                if (buffer.length() == 0) return null;
+                jsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) urlConnection.disconnect();
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("MainActivity", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return jsonStr;
+        }
+
+        @Override
+        public void deliverResult(String data) {
+            super.deliverResult(data);
+        }
+    }
+}
